ddf0472396b2d06adc0815327dc565e726003e99
Madison Walker <madwalker231@gmail.com>
2025-02-03 18:28:35 -0600

    Reinitialized Git with backend and admin folders

---
+++ admin/src/main.jsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react'
+import ReactDom from 'react-dom/client'
+import './index.css'
+import App from './App.jsx'
+import { BrowserRouter } from "react-router-dom"
+
+ReactDom.createRoot(document.getElementById('root')).render(
+  <StrictMode>
+    <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>,
+)
---
+++ backend/index.js
@@ -0,0 +1,134 @@
+const port = 4000;
+const express = require("express");
+const app = express();
+const mongoose = require("mongoose");
+const jwt = require("jsonwebtoken");
+const multer = require("multer");
+const path = require("path");
+const cors = require("cors");
+const { type } = require("os");
+
+app.use(express.json());
+app.use(cors());
+
+// Database connection with MongoDB
+mongoose.connect("mongodb+srv://madwalker231:OiikiJkmJ68KiG1I@cluster0.utnv5.mongodb.net/stables");
+
+// API creation
+
+app.get("/",(req,res)=>{
+    res.send("Express app is running");
+})
+
+// Image storage engine
+
+const storage = multer.diskStorage({
+    destination: './upload/images',
+    filename:(req,file,cb)=>{
+        return cb(null,`${file.fieldname}_${Date.now()}${path.extname(file.originalname)}`);
+    }
+})
+
+const upload = multer({storage:storage})
+
+// Upload endpoint
+app.use('/images',express.static('upload/images'))
+app.post("/upload", upload.single('product'),(req, res)=>{
+    res.json({
+        success:1,
+        image_url:`http://localhost:${port}/images/${req.file.filename}`
+    })
+})
+
+// Schema for creating products
+
+const Products = mongoose.model("Product", {
+    id:{
+        type: Number,
+        required: true,
+    },
+    name:{
+        type: String,
+        required: true,
+    },
+    image:{
+        type: String,
+        required: true,
+    },
+    category:{
+        type: String,
+        required: true,
+    },
+    new_price:{
+        type: Number,
+        required: true,
+    },
+    old_price:{
+        type: Number,
+        required: true,
+    },
+    date:{
+        type: Date,
+        default:Date.now,
+    },
+    available:{
+        type: Boolean,
+        default: true,
+    }
+})
+
+app.post('/addproduct', async(req, res)=>{
+    let products = await Products.find({});
+    let id;
+    if (products.length>0){
+        let last_product_array = products.slice(-1);
+        let last_product = last_product_array[0];
+        id = last_product.id+1;
+    }
+    else{
+        id=1;
+    }
+    const product = new Products({
+        id:id,
+        name:req.body.name,
+        image:req.body.image,
+        category:req.body.category,
+        new_price:req.body.new_price,
+        old_price:req.body.old_price,
+    });
+    console.log(product);
+    await product.save();
+    console.log("Saved");
+    res.json({
+        success:true,
+        name:req.body.name,
+    })
+})
+
+// API for deleting products
+app.post('/removeproduct',async (req,res)=>{
+    await Products.findOneAndDelete({
+        id:req.body.id
+    });
+    console.log("Removed");
+    res.json({
+        success:true,
+        name:req.body.name
+    })
+});
+
+// API for getting products
+app.get('/allproducts', async (req,res)=>{
+    let products = await Products.find({});
+    console.log("All products fetched");
+    res.send(products);
+});
+
+app.listen(port, (error)=>{
+    if(!error){
+        console.log("Server Running on Port: "+port);
+    }
+    else{
+        console.log("Error : "+error);
+    }
+});
