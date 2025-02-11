3ff246181e35aeab6e35cbc8c37dde608010bf22
Madison Walker <madwalker231@gmail.com>
2025-02-11 17:02:04 -0600

    Implemented user endpoints. Stared frontend updates for user account framework.

--- backend/index.js
+++ backend/index.js
@@ -124,6 +124,82 @@ app.get('/allproducts',async (req,res)=>{
     res.send(products);
 });
 
+// User API Shema
+
+const Users = mongoose.model('Users', {
+    name:{
+        type:String,
+    },
+    email:{
+        type:String,
+        unique:true,
+    },
+    password:{
+        type:String,
+    },
+    cartData:{
+        type:Object,
+    },
+    date:{
+        type:Date,
+        default:Date.now,
+    }
+})
+
+// Create User Endpoint
+
+app.post('/signup', async (req,res)=>{
+    let check = await Users.findOne({email:req.body.email});
+    if (check) {
+        return res.status(400).json({success:false,error:"Email already associated with an account"});
+    }
+    let cart = {};
+    for (let i = 0; i < 300; i++) {
+        cart[i] = 0;
+    }
+    const user = new Users({
+        name:req.body.name,
+        email:req.body.email,
+        password:req.body.password,
+        cartData:cart,
+    })
+
+    await user.save();
+
+    const data = {
+        user:{
+            id:user.id
+        }
+    }
+
+    const token = jwt.sign(data,'secret_ecom');
+    res.json({success:true,token});
+})
+
+// User login Endpoint
+
+app.post('/login',async (req,res)=>{
+    let user = await Users.findOne({email:req.body.email});
+    if (user) {
+        const passCompare = req.body.password === user.password;
+        if (passCompare) {
+            const data = {
+                user:{
+                    id:user.id
+                }
+            }
+            const token = jwt.sign(data,'secrete_ecom');
+            res.json({success:true,token});
+        }
+        else {
+            res.json({success:false,error:"Password is invalid"});
+        }
+    }
+    else {
+        res.json({success:false,error:"Email is invalid"});
+    }
+})
+
 app.listen(port, (error)=>{
     if(!error){
         console.log("Server Running on Port: "+port);
--- frontend/src/Pages/LoginSignup.jsx
+++ frontend/src/Pages/LoginSignup.jsx
@@ -1,19 +1,25 @@
+import { useState } from 'react'
 import React from 'react'
 import './CSS/LoginSignup.css'
 
 const LoginSignup = () => {
+    const [state,setState] = useState("Login");
+
     return (
         <div className='loginsignup'>
             <div className="loginsignup-container">
-                <h1>Sign Up</h1>
+                <h1>{state}</h1>
                 <div className="loginsignup-fields">
-                    <input type="text" placeholder='Your Name'/>
+                    {state==="Sign Up"?<input type="text" placeholder='Your Name'/>:<></>}
                     <input type="email" placeholder='Email Address'/>
                     <input type="password" placeholder='Password' />
                 </div>
                 <button>Continue</button>
                 <p className="loginsignup-login">Already have an account? 
                     <span> Login Here</span>
+                </p>
+                <p className="loginsignup-login">Need to create an account? 
+                    <span> Sign Up Here</span>
                 </p>
                 <div className="loginsignup-agree">
                     <input type="checkbox" name='' id=''/>
